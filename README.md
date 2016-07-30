8fe15b0cc1b8465131e8401a3eea1e5f17d53a40
Peter Damoc <pdamoc@gmail.com>
2016-07-30 18:41:20 +0300

    clean-up

--- README.md
+++
@@ -1,4 +0,0 @@
-# Elm assimilation tools
- 
-Convert HTML and CSS to Elm constructs. 
-
--- src/Borg.elm
+++
@@ -1,9 +0,0 @@
-module Borg exposing (..)
-
-import Native.CssHelper
-import Html exposing (Html)
-
-
-withStyle : String -> Html msg -> Html msg
-withStyle =
-    Native.CssHelper.withStyle
